31dcc9029569aecf18e40cd8e164c309909dcaa8
matthewconnellycharles <m.connelly@thecharlesnyc.com>
2016-07-22 16:54:52 -0400

    initial class adding in grid generation

---
+++ src/js/rowcreator.js
@@ -0,0 +1,29 @@
+// get tiles
+var t = document.getElementsByClassName('t');
+
+for (var i=0;i<8;i++){
+	if(i<4){
+		t[i*1].className += ' color-1';
+		t[i*2].className += ' color-2';
+	} else {
+		t[i*1].className += ' color-1';
+		// t[i*1].className += ' color-2';
+	}
+}
+
+// for (var i=0; i<8; i++) {
+
+// }
+//  if (i<4) {
+// 	 if (i%2==0) {
+
+// 	 } else {
+// 		.t1.wq.hh.color2
+// 	}
+// } else {
+// 	 if (i%2==0) {
+// 		.t1.wq.hh.color2
+// 	} else {
+// 		.t1.wq.hh.color1
+// 	}
+// }
