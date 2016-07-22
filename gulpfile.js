08f605bfaf03f97489ebbedf0a03d64d027d079c
matthewconnellycharles <m.connelly@thecharlesnyc.com>
2016-07-22 16:01:50 -0400

    initial commit; work on grid generation in newtiles.jade and ordering.scss

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include Our Plugins
+var sass = require('gulp-sass');
+var jade = require('gulp-jade');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+
+// Compile Our Sass
+gulp.task('sass', function() {
+    return gulp.src('src/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('build/css'));
+});
+
+// Compile Our Jade
+gulp.task('jade', function() {
+    return gulp.src('src/jade/!(_)*.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('build'));
+});
+
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('build/js'))
+        .pipe(rename('app.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('assets', function() {
+    return gulp.src('src/assets/*.jpg')
+        .pipe(gulp.dest('build/assets'));
+});
+
+// Watch Files For Changes
+gulp.task('watch', function() {
+    gulp.watch('src/js/*.js', ['scripts']);
+    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch('src/jade/*.jade', ['jade']);
+});
+
+// Default Task
+gulp.task('default', ['sass','jade','scripts','assets','watch']);
---
+++ src/js/animations.js
@@ -0,0 +1,117 @@
+$(document).keyup(function(e) {
+    switch(e.which) {
+        case 37: //left
+        	classShow();
+        	break;
+
+        case 38: //up
+        	restartZoom();
+        	break;
+
+        case 39: //right
+        	break;
+        	classHide();
+        case 40: //down
+        	reverseZoom();
+
+        	break;
+
+        case 32: //space
+
+
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+// development fn's
+var tile = document.getElementsByClassName('tile');
+function classShow(){
+	for(var i=0;i< tile.length;i++){
+		tile[i].innerHTML=tile[i].className;
+	}
+}
+
+function classHide(){
+	for(var i=0;i< tile.length;i++){
+		tile[i].innerHTML='';
+	}
+}
+
+// animations
+var zoom = new TimelineLite();
+function restartZoom(){return zoom.restart();}
+function reverseZoom(){return zoom.reverse();}
+
+// driectional animations (from x to x, determines direction)
+var up_down = new TimelineLite();
+var down_up = new TimelineLite();
+var left_right = new TimelineLite();
+var right_left= new TimelineLite();
+var tl = new TimelineLite();
+
+var directional = new TimelineLite();
+
+// starting transition
+zoom.fromTo('.slide', 1, 
+	{height:'50vh', width:'50%', opacity: 0.4, y:'50%'}, 
+	{height:'100vh', width:'100%', opacity: 1, y:'0%'});
+
+up_down.fromTo('.up-down', 1, {y:'-100%'}, {y:'0%'});
+down_up.fromTo('.down-up', 1, {y:'100%'}, {y:'0%'});
+left_right.fromTo('.left-right', 1, {x:'-100%'}, {x:'0%'});
+right_left.fromTo('.right-left', 1, {x:'100%'}, {x:'0%'});
+
+function allAnim(){
+	directional.add(up_down)
+		.add(down_up)
+		.add(left_right)
+		.add(right_left);
+	}
+
+var slideCounter = 0;
+
+document.addEventListener('click',function(){
+	slideCounter++;
+	console.log(slideCounter);
+
+	// slide 2
+	if(slideCounter==1){
+
+		reverseZoom();
+
+		setTimeout(function(){
+		 	left_right.fromTo('.left-right', 1, {x:'0%'}, {x:'-100%'});
+			right_left.fromTo('.right-left', 1, {x:'0%'}, {x:'-100%'});
+			tl.fromTo('.col-6.row-3', 1, {x:'0%'}, {x:'-300%'});
+		},1500)
+
+		setTimeout(function(){
+			restartZoom();
+		},2000)
+
+	}
+
+	// slide 3
+	if(slideCounter==2){
+		// alert('text TEST2')
+		slideCounter = 0;
+		zoom.reverse();
+
+		setTimeout(function(){
+			down_up.reverse();
+			left_right.reverse();
+			right_left.reverse();
+			tl.reverse();
+			zoom.reverse();
+		},1000)
+
+		setTimeout(function(){
+			zoom.restart();
+		},1500)
+
+	}
+
+})
